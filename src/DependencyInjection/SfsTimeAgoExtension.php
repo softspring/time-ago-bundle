021c137396e15bfe0ae6dc480b706cacfc45abb0
Javi H. Gil <javihgil@softspring.es>
2021-12-12 08:51:32 +0100

    Refactor to new bundle structure

--- src/DependencyInjection/SfsTimeAgoExtension.php
+++ src/DependencyInjection/SfsTimeAgoExtension.php
@@ -11,7 +11,7 @@ class SfsTimeAgoExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         $loader->load('services.yaml');
     }
 }
--- src/SfsTimeAgoBundle.php
+++ src/SfsTimeAgoBundle.php
@@ -6,5 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SfsTimeAgoBundle extends Bundle
 {
-
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
 }
