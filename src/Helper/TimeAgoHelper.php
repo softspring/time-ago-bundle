aaa350da1ed464b1dc8bf29b4221c7d6a5a157b0
Javi H. Gil <javihgil@softspring.es>
2026-09-23 11:47:38 +0200

    chore: align CI validation standards

--- src/Helper/TimeAgoHelper.php
+++ src/Helper/TimeAgoHelper.php
@@ -33,12 +33,16 @@ class TimeAgoHelper
                 try {
                     $dateTime = new DateTime($dateTime);
                 } catch (Exception) {
-                    $this->logger && $this->logger->warning(sprintf('Timeago extension must receive a valid date string, "%s" received', $dateTime));
+                    if ($this->logger instanceof LoggerInterface) {
+                        $this->logger->warning(sprintf('Timeago extension must receive a valid date string, "%s" received', $dateTime));
+                    }
 
                     return '';
                 }
             } else {
-                $this->logger && $this->logger->warning(sprintf('Timeago extension must receive a DateTime object or string, %s received', gettype($dateTime)));
+                if ($this->logger instanceof LoggerInterface) {
+                    $this->logger->warning(sprintf('Timeago extension must receive a DateTime object or string, %s received', gettype($dateTime)));
+                }
 
                 return '';
             }
